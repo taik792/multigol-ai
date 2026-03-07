5ad10f5d8361f068432e588e9467eb48a5945a1f
taik792 <rosario79@libero.it>
2026-03-07 23:30:08 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -2,41 +2,48 @@ import requests
 import json
 from datetime import datetime, timedelta
 
-API_URL = "https://api.football-data.org/v4/matches"
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
+URL = "https://api.football-data.org/v4/matches"
 
 headers = {
     "X-Auth-Token": API_KEY
 }
 
-response = requests.get(API_URL, headers=headers)
+response = requests.get(URL, headers=headers)
+
 data = response.json()
+
+# controllo sicurezza
+if "matches" not in data:
+    print("ERRORE API:")
+    print(data)
+    exit()
 
 matches = data["matches"]
 
 today = datetime.utcnow().date()
 tomorrow = today + timedelta(days=1)
 
-filtered_matches = []
+filtered = []
 
 for m in matches:
 
     status = m["status"]
 
     match_date = datetime.fromisoformat(
-        m["utcDate"].replace("Z", "+00:00")
+        m["utcDate"].replace("Z","+00:00")
     ).date()
 
     if status != "FINISHED" and (match_date == today or match_date == tomorrow):
 
-        filtered_matches.append({
+        filtered.append({
             "home": m["homeTeam"]["name"],
             "away": m["awayTeam"]["name"],
             "date": m["utcDate"],
             "competition": m["competition"]["name"]
         })
 
-with open("data/matches_today.json", "w") as f:
-    json.dump(filtered_matches, f, indent=4)
+with open("data/matches_today.json","w") as f:
+    json.dump(filtered,f,indent=4)
 
-print("Matches saved:", len(filtered_matches))
+print("Partite salvate:",len(filtered))
