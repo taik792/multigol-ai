a1cb6bbc181d824a47bb01ba70f6daa1d5c5ec17
taik792 <rosario79@libero.it>
2026-03-08 00:48:38 +0100

    Update odds_api_scraper.py

--- odds_api_scraper.py
+++ odds_api_scraper.py
@@ -1,44 +1,44 @@
 import json
 import requests
+from datetime import datetime
 
-API_KEY = "37ddec86e8578a1ff3127d5c394da749"
-
-url = "https://v3.football.api-sports.io/odds"
+API_KEY = "b90932e65c14be06a870fd50fcd20ddc"
 
 headers = {
     "x-apisports-key": API_KEY
 }
 
-# leggiamo le partite
-with open("data/matches_today.json") as f:
-    matches = json.load(f)
+today = datetime.today().strftime('%Y-%m-%d')
+
+url = "https://v3.football.api-sports.io/odds"
+
+params = {
+    "date": today
+}
+
+response = requests.get(url, headers=headers, params=params)
+data = response.json()
 
 odds = {}
 
-for match in matches:
+for game in data.get("response", []):
 
-    home = match["home"]
-    away = match["away"]
-
-    params = {
-        "bookmaker": "8",   # Bet365
-        "league": "135",    # esempio Serie A
-        "season": "2024"
-    }
-
-    response = requests.get(url, headers=headers, params=params)
-
-    data = response.json()
+    home = game["teams"]["home"]["name"]
+    away = game["teams"]["away"]["name"]
 
     key = f"{home} vs {away}"
 
     quote = None
 
-    for game in data.get("response", []):
-        for bookmaker in game.get("bookmakers", []):
-            for bet in bookmaker.get("bets", []):
-                if bet["name"] == "Goals Over/Under":
-                    quote = bet["values"][0]["odd"]
+    for bookmaker in game.get("bookmakers", []):
+        for bet in bookmaker.get("bets", []):
+
+            if bet["name"] == "Goals Over/Under":
+
+                for value in bet["values"]:
+
+                    if value["value"] == "Over 2.5":
+                        quote = value["odd"]
 
     odds[key] = {
         "multigol_2_4": quote
@@ -47,4 +47,4 @@ for match in matches:
 with open("quotes/odds.json", "w") as f:
     json.dump(odds, f, indent=4)
 
-print("Odds reali scaricate:", len(odds))
+print("Odds scaricate:", len(odds))
