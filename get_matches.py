bb397aaa01db316ee92022dd6a8b6b71770f9d61
taik792 <rosario79@libero.it>
2026-03-07 20:34:49 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -1,50 +1,45 @@
 import requests
 import json
+import os
+from datetime import datetime
 
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
-url = "https://v3.football.api-sports.io/fixtures?next=20"
+today = datetime.today().strftime('%Y-%m-%d')
+
+url = f"https://v3.football.api-sports.io/fixtures?date={today}"
 
 headers = {
     "x-apisports-key": API_KEY,
     "x-apisports-host": "v3.football.api-sports.io"
 }
 
-# Richiesta API
 response = requests.get(url, headers=headers)
 data = response.json()
 
-print("API RESPONSE:", data)
-
-# Controllo se la risposta è corretta
-if "response" not in data:
-    print("Errore API o limite richieste")
-    exit()
-
 matches = []
 
-for game in data["response"]:
+if "response" in data:
 
-    home = game["teams"]["home"]["name"]
-    away = game["teams"]["away"]["name"]
+    for game in data["response"]:
 
-    home_goals_avg = 1.5
-    away_goals_avg = 1.2
+        home = game["teams"]["home"]["name"]
+        away = game["teams"]["away"]["name"]
+        league = game["league"]["name"]
 
-    home_conceded = 1.2
-    away_conceded = 1.4
+        matches.append({
+            "home": home,
+            "away": away,
+            "league": league,
+            "home_goals_avg": 1.5,
+            "away_goals_avg": 1.3,
+            "home_conceded": 1.1,
+            "away_conceded": 1.2
+        })
 
-    matches.append({
-        "home": home,
-        "away": away,
-        "home_goals_avg": home_goals_avg,
-        "away_goals_avg": away_goals_avg,
-        "home_conceded": home_conceded,
-        "away_conceded": away_conceded
-    })
+os.makedirs("data", exist_ok=True)
 
-# Salva file per il motore multigol
 with open("data/matches_today.json", "w") as f:
     json.dump(matches, f, indent=4)
 
-print("Matches salvati in data/matches_today.json")
+print("Matches aggiornati")
