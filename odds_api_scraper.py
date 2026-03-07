55f8161d1cb9c6de949c02788bfefca412b0b8f9
taik792 <rosario79@libero.it>
2026-03-08 00:58:31 +0100

    Update odds_api_scraper.py

--- odds_api_scraper.py
+++ odds_api_scraper.py
@@ -1,6 +1,5 @@
 import json
 import requests
-from datetime import datetime
 
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
@@ -8,37 +7,32 @@ headers = {
     "x-apisports-key": API_KEY
 }
 
-today = datetime.today().strftime('%Y-%m-%d')
-
-url = "https://v3.football.api-sports.io/odds"
-
-params = {
-    "date": today
-}
-
-response = requests.get(url, headers=headers, params=params)
-data = response.json()
+# leggiamo le partite trovate prima
+with open("data/matches_today.json") as f:
+    matches = json.load(f)
 
 odds = {}
 
-for game in data.get("response", []):
+url = "https://v3.football.api-sports.io/odds"
 
-    home = game["teams"]["home"]["name"]
-    away = game["teams"]["away"]["name"]
+response = requests.get(url, headers=headers)
+data = response.json()
+
+for match in matches:
+
+    home = match["home"]
+    away = match["away"]
 
     key = f"{home} vs {away}"
 
     quote = None
 
-    for bookmaker in game.get("bookmakers", []):
-        for bet in bookmaker.get("bets", []):
-
-            if bet["name"] == "Goals Over/Under":
-
-                for value in bet["values"]:
-
-                    if value["value"] == "Over 2.5":
-                        quote = value["odd"]
+    for game in data.get("response", []):
+        if "bookmakers" in game:
+            for bookmaker in game["bookmakers"]:
+                for bet in bookmaker["bets"]:
+                    if bet["name"] == "Goals Over/Under":
+                        quote = bet["values"][0]["odd"]
 
     odds[key] = {
         "multigol_2_4": quote
