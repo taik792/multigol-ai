4d0956187d046541c211bb4b465aee7e737c3452
taik792 <rosario79@libero.it>
2026-03-08 00:34:30 +0100

    Update odds_api_scraper.py

--- odds_api_scraper.py
+++ odds_api_scraper.py
@@ -1,23 +1,27 @@
+import requests
 import json
-import random
 
-with open("data/matches_today.json") as f:
-    matches = json.load(f)
+API_KEY = "b90932e65c14be06a870fd50fcd20ddc"
+
+url = f"https://api.the-odds-api.com/v4/sports/soccer/odds/?regions=eu&markets=totals&apiKey={API_KEY}"
+
+response = requests.get(url)
+data = response.json()
 
 odds = {}
 
-for m in matches:
+for match in data:
 
-    home = m["home"]
-    away = m["away"]
+    home = match["home_team"]
+    away = match["away_team"]
 
     key = f"{home} vs {away}"
 
     odds[key] = {
-        "multigol_2_4": round(random.uniform(1.40, 2.10), 2)
+        "multigol_2_4": 1.70
     }
 
 with open("quotes/odds.json", "w") as f:
     json.dump(odds, f, indent=4)
 
-print("Odds generate:", len(odds))
+print("Odds salvate:", len(odds))
