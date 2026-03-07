5235b6bda2ba9e31244ad7c4afe646d852401ea9
taik792 <rosario79@libero.it>
2026-03-07 21:10:37 +0100

    Update odds_api_scraper.py

--- odds_api_scraper.py
+++ odds_api_scraper.py
@@ -4,7 +4,7 @@ import os
 
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
-url = "https://api.the-odds-api.com/v4/sports/soccer_epl/odds"
+url = "https://api.the-odds-api.com/v4/sports/soccer/odds"
 
 params = {
     "apiKey": API_KEY,
@@ -17,9 +17,9 @@ response = requests.get(url, params=params)
 
 data = response.json()
 
-print("Odds ricevute")
-
 os.makedirs("quotes", exist_ok=True)
 
 with open("quotes/odds.json", "w") as f:
     json.dump(data, f, indent=4)
+
+print("Quote aggiornate")
