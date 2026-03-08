346c42abcca5e0bd1290b9362924aa58fc599660
taik792 <rosario79@libero.it>
2026-03-08 13:36:18 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -3,21 +3,20 @@ import json
 from datetime import datetime
 
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
-BASE_URL = "https://v3.football.api-sports.io/fixtures"
+
+url = "https://v3.football.api-sports.io/fixtures"
 
 headers = {
     "x-apisports-key": API_KEY
 }
 
-# data di oggi
 today = datetime.utcnow().strftime("%Y-%m-%d")
 
 params = {
     "date": today
 }
 
-response = requests.get(BASE_URL, headers=headers, params=params)
-
+response = requests.get(url, headers=headers, params=params)
 data = response.json()
 
 matches = []
@@ -28,26 +27,22 @@ if "response" in data:
 
         status = match["fixture"]["status"]["short"]
 
-        # SOLO PARTITE NON INIZIATE
         if status == "NS":
 
             home = match["teams"]["home"]["name"]
             away = match["teams"]["away"]["name"]
-
             league = match["league"]["name"]
             time = match["fixture"]["date"]
 
             matches.append({
-                "league": league,
                 "home": home,
                 "away": away,
+                "league": league,
                 "date": time
             })
 
-# MAX 10 PARTITE
 matches = matches[:10]
 
-# salva file
 with open("data/matches_today.json", "w") as f:
     json.dump(matches, f, indent=4)
 
