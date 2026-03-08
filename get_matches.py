a6ec6d47ac6fefc54db236258b5f237dfe9fcd13
taik792 <rosario79@libero.it>
2026-03-08 19:21:19 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -1,9 +1,8 @@
 import requests
-import os
+import json
+from datetime import datetime
 
-API_KEY = os.getenv("37ddec86e8578a1ff3127d5c394da749")
-
-print("API KEY:", API_KEY)
+API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
 url = "https://v3.football.api-sports.io/fixtures"
 
@@ -11,12 +10,40 @@ headers = {
     "x-apisports-key": API_KEY
 }
 
+today = datetime.utcnow().strftime("%Y-%m-%d")
+
 params = {
-    "league": 135,
-    "season": 2024
+    "date": today
 }
 
 response = requests.get(url, headers=headers, params=params)
+data = response.json()
 
-print("STATUS:", response.status_code)
-print(response.text[:500])
+matches = []
+
+if "response" in data:
+
+    for match in data["response"]:
+
+        status = match["fixture"]["status"]["short"]
+
+        if status == "NS":
+
+            home = match["teams"]["home"]["name"]
+            away = match["teams"]["away"]["name"]
+            league = match["league"]["name"]
+            time = match["fixture"]["date"]
+
+            matches.append({
+                "home": home,
+                "away": away,
+                "league": league,
+                "date": time
+            })
+
+matches = matches[:10]
+
+with open("data/matches_today.json", "w") as f:
+    json.dump(matches, f, indent=4)
+
+print("Partite salvate:", len(matches))
