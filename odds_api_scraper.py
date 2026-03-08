1e57037a1f9f29bb08d8efdc96f7fa7d68dd3fb7
taik792 <rosario79@libero.it>
2026-03-08 08:34:56 +0100

    Update odds_api_scraper.py

--- odds_api_scraper.py
+++ odds_api_scraper.py
@@ -9,43 +9,62 @@ headers = {
 
 url = "https://v3.football.api-sports.io/odds"
 
-response = requests.get(url, headers=headers)
+params = {
+    "league": 39,
+    "season": 2026
+}
+
+response = requests.get(url, headers=headers, params=params)
 data = response.json()
 
 odds = {}
 
-for game in data["response"]:
+for match in data["response"]:
 
-    home = game["teams"]["home"]["name"]
-    away = game["teams"]["away"]["name"]
+    home = match["teams"]["home"]["name"]
+    away = match["teams"]["away"]["name"]
 
     key = f"{home}-{away}"
 
-    over25 = None
-    under25 = None
+    odds[key] = {}
 
-    for bookmaker in game["bookmakers"]:
+    for bookmaker in match["bookmakers"]:
 
         for bet in bookmaker["bets"]:
 
-            if bet["name"] == "Goals Over/Under":
+            name = bet["name"]
+
+            # 1X2
+            if name == "Match Winner":
+
+                for value in bet["values"]:
+
+                    if value["value"] == "Home":
+                        odds[key]["1"] = float(value["odd"])
+
+                    if value["value"] == "Away":
+                        odds[key]["2"] = float(value["odd"])
+
+            # OVER/UNDER
+            if name == "Goals Over/Under":
 
                 for value in bet["values"]:
 
                     if value["value"] == "Over 2.5":
-                        over25 = float(value["odd"])
+                        odds[key]["over25"] = float(value["odd"])
 
                     if value["value"] == "Under 2.5":
-                        under25 = float(value["odd"])
+                        odds[key]["under25"] = float(value["odd"])
 
-    if over25:
+            # BTTS
+            if name == "Both Teams Score":
 
-        odds[key] = {
-            "over25": over25,
-            "under25": under25
-        }
+                for value in bet["values"]:
+
+                    if value["value"] == "Yes":
+                        odds[key]["btts_yes"] = float(value["odd"])
 
 with open("quotes/odds.json","w") as f:
     json.dump(odds,f,indent=4)
 
-print("Quote bookmaker trovate:",len(odds))
+print("Quote salvate:",len(odds))
