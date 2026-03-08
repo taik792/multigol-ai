d96fac587a5612850c887e7d3b35114c136e98e0
taik792 <rosario79@libero.it>
2026-03-08 08:16:44 +0100

    Update multigol_engine.py

--- multigol_engine.py
+++ multigol_engine.py
@@ -19,54 +19,72 @@ for match in matches:
 
     quote = odds.get(key,{})
 
-    over15 = float(quote.get("over15",2.0))
+    home_win = float(quote.get("1",2.5))
+    draw = float(quote.get("X",3.2))
+    away_win = float(quote.get("2",2.8))
     over25 = float(quote.get("over25",2.0))
-    under25 = float(quote.get("under25",2.0))
     btts = float(quote.get("btts_yes",2.0))
 
-    # stima gol
-    goal_score = 0
+    # stimiamo gol totali
+    if over25 < 1.55:
+        total_goals = 3.2
+    elif over25 < 1.75:
+        total_goals = 2.8
+    elif over25 < 2.00:
+        total_goals = 2.5
+    else:
+        total_goals = 2.2
 
-    if over15 < 1.35:
-        goal_score += 1
+    # distribuzione gol casa / ospite
+    if home_win < away_win:
 
-    if over25 < 1.70:
-        goal_score += 1
+        home_expected = total_goals * 0.6
+        away_expected = total_goals * 0.4
 
-    if btts < 1.75:
-        goal_score += 1
+    elif away_win < home_win:
 
-    if under25 < 1.65:
-        goal_score -= 1
+        home_expected = total_goals * 0.4
+        away_expected = total_goals * 0.6
 
-    # scelta multigol
-    if goal_score >= 2:
+    else:
+
+        home_expected = total_goals * 0.5
+        away_expected = total_goals * 0.5
+
+    # multigol totale
+    if total_goals >= 3:
 
         multigol = "2-4"
-        home_goals = random.choice(["1-2","1-3","2-3"])
-        away_goals = random.choice(["1-2","0-2"])
-        confidence = 85
 
-    elif goal_score == 1:
+    elif total_goals >= 2.6:
 
         multigol = "2-3"
-        home_goals = random.choice(["1-2","1-3"])
-        away_goals = random.choice(["1-2"])
-        confidence = 82
 
-    elif goal_score == 0:
+    elif total_goals >= 2.2:
 
         multigol = "1-3"
-        home_goals = random.choice(["0-2","1-2"])
-        away_goals = random.choice(["0-2","1-2"])
-        confidence = 78
 
     else:
 
         multigol = "0-2"
-        home_goals = random.choice(["0-1"])
-        away_goals = random.choice(["0-1"])
-        confidence = 75
+
+    # casa
+    if home_expected >= 1.8:
+        home_goals = "1-3"
+    elif home_expected >= 1.2:
+        home_goals = "1-2"
+    else:
+        home_goals = "0-1"
+
+    # ospite
+    if away_expected >= 1.8:
+        away_goals = "1-3"
+    elif away_expected >= 1.2:
+        away_goals = "1-2"
+    else:
+        away_goals = "0-1"
+
+    confidence = random.randint(78,88)
 
     predictions.append({
         "date": date,
