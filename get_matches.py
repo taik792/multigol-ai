899f7ddc6cf3726a752c702b08aced968d0a1294
taik792 <rosario79@libero.it>
2026-03-09 09:53:33 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -2,46 +2,42 @@ import requests
 import json
 from datetime import datetime
 
-API_KEY = "37ddec86e8578a1ff3127d5c394da749"
+API_KEY="37ddec86e8578a1ff3127d5c394da749"
 
-url = "https://v3.football.api-sports.io/fixtures"
+url="https://v3.football.api-sports.io/fixtures"
 
-headers = {
-    "x-apisports-key": API_KEY
+headers={
+"x-apisports-key":API_KEY
 }
 
-today = datetime.now().strftime("%Y-%m-%d")
+today=datetime.now().strftime("%Y-%m-%d")
 
-params = {
-    "date": today,
-    "timezone": "Europe/Rome"
+params={
+"date":today,
+"timezone":"Europe/Rome"
 }
 
-response = requests.get(url, headers=headers, params=params)
-data = response.json()
+r=requests.get(url,headers=headers,params=params)
+data=r.json()
 
-matches = []
+matches=[]
 
-if "response" in data:
+for m in data["response"]:
 
-    for match in data["response"]:
+    if m["fixture"]["status"]["short"]=="NS":
 
-        status = match["fixture"]["status"]["short"]
+        matches.append({
+            "fixture_id":m["fixture"]["id"],
+            "home_id":m["teams"]["home"]["id"],
+            "away_id":m["teams"]["away"]["id"],
+            "home":m["teams"]["home"]["name"],
+            "away":m["teams"]["away"]["name"],
+            "league":m["league"]["name"],
+            "league_id":m["league"]["id"],
+            "season":m["league"]["season"]
+        })
 
-        if status == "NS":
-
-            home = match["teams"]["home"]["name"]
-            away = match["teams"]["away"]["name"]
-
-            league = match["league"]["name"]
-
-            matches.append({
-                "home": home,
-                "away": away,
-                "league": league
-            })
-
-matches = matches[:10]
+matches=matches[:10]
 
 with open("matches.json","w") as f:
     json.dump(matches,f,indent=4)
