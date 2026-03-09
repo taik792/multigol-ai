8b40ff685049f2e8e6edfde6625856671c3ca2da
taik792 <rosario79@libero.it>
2026-03-09 08:51:45 +0100

    Update get_matches.py
    fix matches

--- get_matches.py
+++ get_matches.py
@@ -2,7 +2,6 @@ import requests
 import json
 from datetime import datetime
 
-# API KEY
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
 url = "https://v3.football.api-sports.io/fixtures"
@@ -27,23 +26,17 @@ if "response" in data:
 
     for match in data["response"]:
 
-        status = match["fixture"]["status"]["short"]
+        home = match["teams"]["home"]["name"]
+        away = match["teams"]["away"]["name"]
 
-        # prendiamo solo partite non iniziate
-        if status in ["NS","TBD"]:
+        matches.append({
+            "home": home,
+            "away": away,
+            "multigol": "2-3",
+            "over25": "Yes",
+            "btts": "Yes"
+        })
 
-            home = match["teams"]["home"]["name"]
-            away = match["teams"]["away"]["name"]
-
-            matches.append({
-                "home": home,
-                "away": away,
-                "multigol": "2-3",
-                "over25": "Yes",
-                "btts": "Yes"
-            })
-
-# salva file per il sito
 with open("matches.json", "w", encoding="utf-8") as f:
     json.dump(matches, f, indent=4, ensure_ascii=False)
 
