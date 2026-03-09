cd0bab039693c181117f1c1acfc445df4cd65f07
taik792 <rosario79@libero.it>
2026-03-09 08:34:06 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -2,7 +2,7 @@ import requests
 import json
 from datetime import datetime
 
-# METTI QUI LA TUA API KEY
+# API KEY
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
 url = "https://v3.football.api-sports.io/fixtures"
@@ -15,7 +15,8 @@ headers = {
 today = datetime.now().strftime("%Y-%m-%d")
 
 params = {
-    "date": today
+    "date": today,
+    "timezone": "Europe/Rome"
 }
 
 response = requests.get(url, headers=headers, params=params)
@@ -23,23 +24,26 @@ data = response.json()
 
 matches = []
 
-# controlla che ci siano partite
-if "response" in data:
+if "response" in data and len(data["response"]) > 0:
 
     for match in data["response"]:
 
-        home = match["teams"]["home"]["name"]
-        away = match["teams"]["away"]["name"]
+        status = match["fixture"]["status"]["short"]
 
-        matches.append({
-            "home": home,
-            "away": away,
-            "multigol": "2-3",
-            "over25": "Yes",
-            "btts": "Yes"
-        })
+        # prendiamo solo partite NON finite
+        if status in ["NS", "TBD"]:
 
-# salva il JSON che il sito legge
+            home = match["teams"]["home"]["name"]
+            away = match["teams"]["away"]["name"]
+
+            matches.append({
+                "home": home,
+                "away": away,
+                "multigol": "2-3",
+                "over25": "Yes",
+                "btts": "Yes"
+            })
+
 with open("matches.json", "w", encoding="utf-8") as f:
     json.dump(matches, f, indent=4, ensure_ascii=False)
 
