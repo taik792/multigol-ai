19435c5fe6422a3bc9071ed1abb25183b39a8beb
taik792 <rosario79@libero.it>
2026-03-11 10:21:47 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
@@ -17,24 +17,38 @@ params = {
 }
 
 response = requests.get(url, headers=headers, params=params)
+
 data = response.json()
 
 matches = []
 
-if "response" in data:
+for m in data["response"]:
 
-    for m in data["response"]:
+    home = m["teams"]["home"]["name"]
+    away = m["teams"]["away"]["name"]
+    league = m["league"]["name"]
 
-        match = {
-            "home": m["teams"]["home"]["name"],
-            "away": m["teams"]["away"]["name"],
-            "league": m["league"]["name"],
-            "time": m["fixture"]["date"][11:16]
-        }
+    # ORARIO PARTITA
+    utc_time = m["fixture"]["date"]
 
-        matches.append(match)
+    dt = datetime.fromisoformat(utc_time.replace("Z", ""))
+    dt = dt + timedelta(hours=1)
+
+    time = dt.strftime("%H:%M")
+
+    match = {
+        "home": home,
+        "away": away,
+        "league": league,
+        "time": time
+    }
+
+    matches.append(match)
+
+# massimo 30 partite
+matches = matches[:30]
 
 with open("data/matches_today.json", "w") as f:
     json.dump(matches, f, indent=4)
 
-print("Partite trovate:", len(matches))
+print("Partite salvate:", len(matches))
