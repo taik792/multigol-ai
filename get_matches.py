d7539f65ce33ec7e4152d238a35be51d6c1a9b4c
taik792 <rosario79@libero.it>
2026-03-12 00:57:44 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -6,37 +6,41 @@ API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
 url = "https://v3.football.api-sports.io/fixtures"
 
-headers = {
-    "x-apisports-key": API_KEY
-}
-
 today = datetime.utcnow().strftime("%Y-%m-%d")
 
-params = {
-    "date": today
+headers = {
+"x-apisports-key": API_KEY
 }
 
-response = requests.get(url, headers=headers, params=params).json()
+params = {
+"date": today
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+data = response.json()
 
 matches = []
 
-for m in response["response"]:
+for m in data["response"]:
 
-    match = {
-        "fixture_id": m["fixture"]["id"],
-        "home": m["teams"]["home"]["name"],
-        "away": m["teams"]["away"]["name"],
-        "home_id": m["teams"]["home"]["id"],
-        "away_id": m["teams"]["away"]["id"],
-        "league": m["league"]["name"],
-        "country": m["league"]["country"],
-        "league_id": m["league"]["id"],
-        "time": m["fixture"]["date"][11:16]
-    }
+    home = m["teams"]["home"]["name"]
+    away = m["teams"]["away"]["name"]
 
-    matches.append(match)
+    league = m["league"]["name"]
+    country = m["league"]["country"]
 
-matches = matches[:30]
+    time = m["fixture"]["date"][11:16]
 
-with open("data/matches_today.json","w") as f:
+    matches.append({
+        "home":home,
+        "away":away,
+        "league":league,
+        "country":country,
+        "time":time
+    })
+
+with open("matches.json","w") as f:
     json.dump(matches,f,indent=2)
+
+print("Partite salvate:",len(matches))
