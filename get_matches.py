e379537c3ba72ff24654b7509138c6fc83f99f1a
taik792 <rosario79@libero.it>
2026-03-11 14:21:13 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -1,9 +1,7 @@
 import requests
 import json
-import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
-# INSERISCI QUI LA TUA API KEY
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
 url = "https://v3.football.api-sports.io/fixtures"
@@ -23,19 +21,28 @@ data = response.json()
 
 matches = []
 
+now = datetime.utcnow()
+
 for m in data["response"]:
 
     status = m["fixture"]["status"]["short"]
 
-    # Prende solo partite non iniziate
+    # solo partite non iniziate
     if status != "NS":
+        continue
+
+    match_time_str = m["fixture"]["date"]
+    match_time = datetime.fromisoformat(match_time_str.replace("Z",""))
+
+    # deve iniziare tra almeno 1 ora
+    if match_time - now < timedelta(hours=1):
         continue
 
     home = m["teams"]["home"]["name"]
     away = m["teams"]["away"]["name"]
     league = m["league"]["name"]
 
-    time = m["fixture"]["date"][11:16]
+    time = match_time.strftime("%H:%M")
 
     matches.append({
         "home": home,
@@ -43,9 +50,6 @@ for m in data["response"]:
         "league": league,
         "time": time
     })
-
-# crea cartella data se non esiste
-os.makedirs("data", exist_ok=True)
 
 with open("data/matches_today.json", "w") as f:
     json.dump(matches, f, indent=2)
