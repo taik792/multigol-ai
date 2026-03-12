7d818e217b7aad121439fe6f8bade2d9a9aab35c
taik792 <rosario79@libero.it>
2026-03-12 09:28:30 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -3,16 +3,14 @@ import json
 
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
+url = "https://v3.football.api-sports.io/fixtures"
+
 headers = {
     "x-apisports-key": API_KEY
 }
 
-url = "https://v3.football.api-sports.io/fixtures"
-
 # leghe principali
 leagues = [39, 135, 140, 78, 61]
-
-season = 2025
 
 matches = []
 
@@ -20,7 +18,8 @@ for league in leagues:
 
     params = {
         "league": league,
-        "season": season
+        "season": 2025,
+        "status": "NS"
     }
 
     response = requests.get(url, headers=headers, params=params)
@@ -36,7 +35,9 @@ for league in leagues:
             "home": m["teams"]["home"]["name"],
             "away": m["teams"]["away"]["name"],
             "league": m["league"]["name"],
-            "time": m["fixture"]["date"]
+            "time": m["fixture"]["date"],
+            "home_id": m["teams"]["home"]["id"],
+            "away_id": m["teams"]["away"]["id"]
         })
 
 print("Partite trovate:", len(matches))
