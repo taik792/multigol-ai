354b15dcef7f625f208caa51c48c4fb66c47a76c
taik792 <rosario79@libero.it>
2026-03-12 08:27:20 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -1,41 +1,54 @@
 import requests
 import json
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 API_KEY = os.getenv("37ddec86e8578a1ff3127d5c394da749")
-
-url = "https://v3.football.api-sports.io/fixtures"
 
 headers = {
     "x-apisports-key": API_KEY
 }
 
+url = "https://v3.football.api-sports.io/fixtures"
+
 today = datetime.utcnow().strftime("%Y-%m-%d")
-
-params = {
-    "date": today
-}
-
-response = requests.get(url, headers=headers, params=params)
-
-data = response.json()
+tomorrow = (datetime.utcnow() + timedelta(days=1)).strftime("%Y-%m-%d")
 
 matches = []
 
-for m in data.get("response", []):
+def get_games(date):
 
-    match = {
-        "home": m["teams"]["home"]["name"],
-        "away": m["teams"]["away"]["name"],
-        "league": m["league"]["name"],
-        "country": m["league"]["country"],
-        "time": m["fixture"]["date"][11:16]
+    params = {
+        "date": date
     }
 
-    matches.append(match)
+    r = requests.get(url, headers=headers, params=params)
 
-with open("matches.json", "w", encoding="utf-8") as f:
-    json.dump(matches, f, indent=2)
+    data = r.json()
 
-print("Partite trovate:", len(matches))
+    if "response" not in data:
+        return []
+
+    games = []
+
+    for m in data["response"]:
+
+        if m["fixture"]["status"]["short"] == "NS":
+
+            games.append({
+                "home": m["teams"]["home"]["name"],
+                "away": m["teams"]["away"]["name"],
+                "league": m["league"]["name"],
+                "time": m["fixture"]["date"]
+            })
+
+    return games
+
+
+matches += get_games(today)
+matches += get_games(tomorrow)
+
+with open("matches.json","w") as f:
+    json.dump(matches,f,indent=4)
+
+print("Partite trovate:",len(matches))
