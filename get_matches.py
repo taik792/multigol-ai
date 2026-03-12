69a04098bb949cbc9f38a622804d858f25d0f70e
taik792 <rosario79@libero.it>
2026-03-12 09:00:03 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -3,7 +3,6 @@ import json
 import os
 from datetime import datetime
 
-# INSERISCI QUI LA TUA API KEY
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
 url = "https://v3.football.api-sports.io/fixtures"
@@ -19,35 +18,23 @@ params = {
 }
 
 response = requests.get(url, headers=headers, params=params)
+
+print("HTTP STATUS:", response.status_code)
+
 data = response.json()
 
 matches = []
 
-for m in data["response"]:
-
-    status = m["fixture"]["status"]["short"]
-
-    # Prende solo partite non iniziate
-    if status != "NS":
-        continue
-
-    home = m["teams"]["home"]["name"]
-    away = m["teams"]["away"]["name"]
-    league = m["league"]["name"]
-
-    time = m["fixture"]["date"][11:16]
+for m in data.get("response", []):
 
     matches.append({
-        "home": home,
-        "away": away,
-        "league": league,
-        "time": time
+        "home": m["teams"]["home"]["name"],
+        "away": m["teams"]["away"]["name"],
+        "league": m["league"]["name"],
+        "time": m["fixture"]["date"]
     })
 
-# crea cartella data se non esiste
-os.makedirs("data", exist_ok=True)
-
-with open("data/matches_today.json", "w") as f:
-    json.dump(matches, f, indent=2)
-
 print("Matches salvati:", len(matches))
+
+with open("matches.json", "w") as f:
+    json.dump(matches, f, indent=4)
