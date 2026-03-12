1b61526e12c52ae88c98875fcdad41c236aa5a7c
taik792 <rosario79@libero.it>
2026-03-12 01:00:51 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -1,19 +1,21 @@
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
 url = "https://v3.football.api-sports.io/fixtures"
 
-today = datetime.utcnow().strftime("%Y-%m-%d")
-
 headers = {
 "x-apisports-key": API_KEY
 }
 
+today = datetime.utcnow().strftime("%Y-%m-%d")
+tomorrow = (datetime.utcnow() + timedelta(days=1)).strftime("%Y-%m-%d")
+
 params = {
-"date": today
+"from": today,
+"to": tomorrow
 }
 
 response = requests.get(url, headers=headers, params=params)
@@ -33,14 +35,14 @@ for m in data["response"]:
     time = m["fixture"]["date"][11:16]
 
     matches.append({
-        "home":home,
-        "away":away,
-        "league":league,
-        "country":country,
-        "time":time
+        "home": home,
+        "away": away,
+        "league": league,
+        "country": country,
+        "time": time
     })
 
-with open("matches.json","w") as f:
-    json.dump(matches,f,indent=2)
+with open("matches.json", "w") as f:
+    json.dump(matches, f, indent=2)
 
-print("Partite salvate:",len(matches))
+print("Partite trovate:", len(matches))
