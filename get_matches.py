eecfb6e840430cfcbed526eeba63e99653392919
taik792 <rosario79@libero.it>
2026-03-12 09:05:59 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -1,41 +1,34 @@
 import requests
 import json
-from datetime import datetime, timedelta
 
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
+
+url = "https://v3.football.api-sports.io/fixtures"
 
 headers = {
     "x-apisports-key": API_KEY
 }
 
-url = "https://v3.football.api-sports.io/fixtures"
+params = {
+    "next": 50
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+print("HTTP STATUS:", response.status_code)
+
+data = response.json()
 
 matches = []
 
-for i in range(3):
+for m in data.get("response", []):
 
-    date = (datetime.utcnow() + timedelta(days=i)).strftime("%Y-%m-%d")
-
-    print("DATA:", date)
-
-    params = {
-        "date": date
-    }
-
-    response = requests.get(url, headers=headers, params=params)
-
-    print("HTTP STATUS:", response.status_code)
-
-    data = response.json()
-
-    for m in data.get("response", []):
-
-        matches.append({
-            "home": m["teams"]["home"]["name"],
-            "away": m["teams"]["away"]["name"],
-            "league": m["league"]["name"],
-            "time": m["fixture"]["date"]
-        })
+    matches.append({
+        "home": m["teams"]["home"]["name"],
+        "away": m["teams"]["away"]["name"],
+        "league": m["league"]["name"],
+        "time": m["fixture"]["date"]
+    })
 
 print("Partite trovate:", len(matches))
 
