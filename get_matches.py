23a70a53f88bf1aae384873614c0e2cc26893348
taik792 <rosario79@libero.it>
2026-03-12 08:20:51 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -1,5 +1,7 @@
 import requests
+import json
 import os
+from datetime import datetime
 
 API_KEY = os.getenv("37ddec86e8578a1ff3127d5c394da749")
 
@@ -9,12 +11,31 @@ headers = {
     "x-apisports-key": API_KEY
 }
 
+today = datetime.utcnow().strftime("%Y-%m-%d")
+
 params = {
-    "next": 10
+    "date": today
 }
 
 response = requests.get(url, headers=headers, params=params)
 
-print("HTTP STATUS:", response.status_code)
-print("RISPOSTA API:")
-print(response.text)
+data = response.json()
+
+matches = []
+
+for m in data.get("response", []):
+
+    match = {
+        "home": m["teams"]["home"]["name"],
+        "away": m["teams"]["away"]["name"],
+        "league": m["league"]["name"],
+        "country": m["league"]["country"],
+        "time": m["fixture"]["date"][11:16]
+    }
+
+    matches.append(match)
+
+with open("matches.json", "w", encoding="utf-8") as f:
+    json.dump(matches, f, indent=2)
+
+print("Partite trovate:", len(matches))
