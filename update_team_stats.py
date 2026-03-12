c81fa8cc706516c676f574527cc533787c80dd6c
taik792 <rosario79@libero.it>
2026-03-12 01:16:45 +0100

    Update update_team_stats.py

--- update_team_stats.py
+++ update_team_stats.py
@@ -4,42 +4,60 @@ import json
 API_KEY = "37ddec86e8578a1ff3127d5c394da749"
 
 headers = {
- "x-apisports-key": API_KEY
+    "x-apisports-key": API_KEY
 }
 
-with open("data/matches_today.json") as f:
- matches = json.load(f)
+with open("matches.json") as f:
+    matches = json.load(f)
 
-stats = {}
+try:
+    with open("teams_stats.json") as f:
+        stats = json.load(f)
+except:
+    stats = {}
 
 for m in matches:
 
- for team in [("home_id","home"),("away_id","away")]:
+    for team in [m["home"], m["away"]]:
 
-    team_id = m[team[0]]
-    league_id = m["league_id"]
+        if team in stats:
+            continue
 
-    url = "https://v3.football.api-sports.io/teams/statistics"
+        url = "https://v3.football.api-sports.io/teams"
+        params = {"search": team}
 
-    params = {
-     "team": team_id,
-     "league": league_id,
-     "season": 2024
-    }
+        r = requests.get(url, headers=headers, params=params)
+        data = r.json()
 
-    r = requests.get(url,headers=headers,params=params).json()
+        if not data["response"]:
+            continue
 
-    if r["response"]:
+        team_id = data["response"][0]["team"]["id"]
 
-        data = r["response"]
-
-        scored = data["goals"]["for"]["average"]["total"]
-        conceded = data["goals"]["against"]["average"]["total"]
-
-        stats[team_id] = {
-          "scored": float(scored),
-          "conceded": float(conceded)
+        url = "https://v3.football.api-sports.io/teams/statistics"
+        params = {
+            "team": team_id,
+            "league": 39,
+            "season": 2024
         }
 
-with open("data/teams_stats.json","w") as f:
- json.dump(stats,f,indent=2)
+        r = requests.get(url, headers=headers, params=params)
+        data = r.json()
+
+        if not data["response"]:
+            continue
+
+        g = data["response"]["goals"]
+
+        scored = g["for"]["average"]["total"]
+        conceded = g["against"]["average"]["total"]
+
+        stats[team] = {
+            "scored": float(scored),
+            "conceded": float(conceded)
+        }
+
+        print("Stats salvate:", team)
+
+with open("teams_stats.json", "w") as f:
+    json.dump(stats, f, indent=2)
