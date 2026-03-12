c3477123b9a5d4168cfd3e6e4ee289830b9e8113
taik792 <rosario79@libero.it>
2026-03-12 08:30:44 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -1,54 +1,47 @@
 import requests
 import json
-import os
 from datetime import datetime, timedelta
 
-API_KEY = os.getenv("37ddec86e8578a1ff3127d5c394da749")
+API_KEY = "37ddec86e8578a1ff3127d5c394da749"
+
+url = "https://v3.football.api-sports.io/fixtures"
 
 headers = {
     "x-apisports-key": API_KEY
 }
 
-url = "https://v3.football.api-sports.io/fixtures"
+now = datetime.utcnow()
+tomorrow = now + timedelta(days=1)
 
-today = datetime.utcnow().strftime("%Y-%m-%d")
-tomorrow = (datetime.utcnow() + timedelta(days=1)).strftime("%Y-%m-%d")
+params = {
+    "from": now.strftime("%Y-%m-%d"),
+    "to": tomorrow.strftime("%Y-%m-%d")
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+print("HTTP STATUS:", response.status_code)
+
+data = response.json()
 
 matches = []
 
-def get_games(date):
-
-    params = {
-        "date": date
-    }
-
-    r = requests.get(url, headers=headers, params=params)
-
-    data = r.json()
-
-    if "response" not in data:
-        return []
-
-    games = []
+if "response" in data:
 
     for m in data["response"]:
 
-        if m["fixture"]["status"]["short"] == "NS":
+        status = m["fixture"]["status"]["short"]
 
-            games.append({
+        if status == "NS":
+
+            matches.append({
                 "home": m["teams"]["home"]["name"],
                 "away": m["teams"]["away"]["name"],
                 "league": m["league"]["name"],
                 "time": m["fixture"]["date"]
             })
 
-    return games
+print("Partite trovate:", len(matches))
 
-
-matches += get_games(today)
-matches += get_games(tomorrow)
-
-with open("matches.json","w") as f:
-    json.dump(matches,f,indent=4)
-
-print("Partite trovate:",len(matches))
+with open("matches.json", "w") as f:
+    json.dump(matches, f, indent=4)
