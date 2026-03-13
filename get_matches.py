fa06c93951dc98904e2dd1ddc1cae4e7cb0af969
taik792 <rosario79@libero.it>
2026-03-13 08:52:29 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -5,20 +5,21 @@ from datetime import datetime
 
 API_KEY = os.getenv("API_KEY")
 
+url = "https://v3.football.api-sports.io/fixtures"
+
 headers = {
     "x-apisports-key": API_KEY
 }
 
 today = datetime.utcnow().strftime("%Y-%m-%d")
 
-url = "https://v3.football.api-sports.io/fixtures"
-
 params = {
-    "date": today
+    "date": today,
+    "season": 2025
 }
 
-r = requests.get(url, headers=headers, params=params)
-data = r.json()
+response = requests.get(url, headers=headers, params=params)
+data = response.json()
 
 matches = []
 
@@ -27,18 +28,22 @@ for m in data["response"]:
     if m["fixture"]["status"]["short"] != "NS":
         continue
 
-    matches.append({
+    match = {
         "home": m["teams"]["home"]["name"],
         "away": m["teams"]["away"]["name"],
         "home_id": m["teams"]["home"]["id"],
         "away_id": m["teams"]["away"]["id"],
         "league": m["league"]["name"],
-        "league_id": m["league"]["id"]
-    })
+        "country": m["league"]["country"],
+        "league_id": m["league"]["id"],
+        "date": m["fixture"]["date"]
+    }
 
-print("Partite trovate:", len(matches))
+    matches.append(match)
 
 os.makedirs("data", exist_ok=True)
 
-with open("data/matches.json","w") as f:
-    json.dump(matches,f,indent=4)
+with open("data/matches.json", "w") as f:
+    json.dump(matches, f, indent=2)
+
+print("Partite trovate:", len(matches))
