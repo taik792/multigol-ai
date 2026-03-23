1b428c6cd0cae992768e720a879afd6a9ebf8709
taik792 <rosario79@libero.it>
2026-03-23 20:12:52 +0100

    Update get_matches.py

--- get_matches.py
+++ get_matches.py
@@ -1,13 +1,16 @@
 import requests
 import json
-from datetime import datetime
 import os
+from datetime import datetime, timedelta
 
 API_KEY = os.getenv("API_KEY")
 
 url = "https://v3.football.api-sports.io/fixtures"
 
-today = datetime.now().strftime("%Y-%m-%d")
+now = datetime.utcnow() + timedelta(hours=2)
+today = now.strftime("%Y-%m-%d")
+
+print("DATA USATA:", today)
 
 headers = {
     "x-apisports-key": API_KEY
@@ -21,23 +24,30 @@ params = {
 response = requests.get(url, headers=headers, params=params)
 data = response.json()
 
+# 🧹 pulizia
+if os.path.exists("data/matches_today.json"):
+    os.remove("data/matches_today.json")
+
 matches = []
 
 for m in data["response"]:
-    try:
-        match = {
-            "fixture_id": m["fixture"]["id"],
-            "home": m["teams"]["home"]["name"],
-            "away": m["teams"]["away"]["name"],
-            "date": m["fixture"]["date"],
-            "league": m["league"]["name"],
-            "country": m["league"]["country"]
-        }
-        matches.append(match)
-    except:
+
+    # 🔥 SOLO NON INIZIATE
+    if m["fixture"]["status"]["short"] != "NS":
         continue
+
+    match = {
+        "fixture_id": m["fixture"]["id"],
+        "home": m["teams"]["home"]["name"],
+        "away": m["teams"]["away"]["name"],
+        "date": m["fixture"]["date"],
+        "league": m["league"]["name"],
+        "country": m["league"]["country"]
+    }
+
+    matches.append(match)
 
 with open("data/matches_today.json", "w") as f:
     json.dump(matches, f, indent=2)
 
-print(f"Scaricate {len(matches)} partite di oggi")
+print(f"Scaricate {len(matches)} partite NUOVE")
