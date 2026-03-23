f7eb341697449abf1462130badbf00810231ae3f
taik792 <rosario79@libero.it>
2026-03-23 19:29:58 +0100

    Update run_smart.py

--- run_smart.py
+++ run_smart.py
@@ -3,11 +3,12 @@ from datetime import datetime
 
 hour = datetime.utcnow().hour
 
-# Italia UTC+1 (ok per ora)
+# Italia UTC+1
 italy_hour = (hour + 1) % 24
 
 print(f"Ora Italia: {italy_hour}")
 
+# SOLO 2 RUN
 if italy_hour not in [10, 16]:
     print("⛔ Skip per risparmio API")
     exit()
